9bd776e8606c2a8d06ec17ef5090f5b3389d7cbd
Kamil Kubiak <kamil.kubiak@po.policja.gov.pl>
2022-05-25 13:53:40 +0200

    Zmiana klasy i dodanie metod

--- function/write.py
+++ function/write.py
@@ -4,10 +4,12 @@ import tkinter as tk
 
 
 class Password:
+    INPUT = None
+    TEXT_INFO = None
 
     def open_frame(self):
         frame = tk.Toplevel()
-        frame.geometry('200x150')
+        frame.geometry('250x150')
         frame.title('Sprawdz swoje chasło')
         frame.columnconfigure(1, weight=2)
         frame.rowconfigure(1, weight=1)
@@ -19,23 +21,28 @@ class Password:
         enter_pass.config(show='*')
         enter_pass.grid(column=0, row=1, columnspan=2, sticky=tk.N)
 
-        def take_password():
-            password = Password()
-            return enter_pass.get()
+        self.INPUT = enter_pass
+        self.TEXT_INFO = tk.StringVar()
+        self.TEXT_INFO.set('*** tu znajdzie się analiza hasła ***')
 
-        analys_pass = tk.Label(master=frame, font=('Comic sans MS', 12), text='a tra lalala')
+        analys_pass = tk.Label(master=frame, font=('Comic sans MS', 10), textvariable=self.TEXT_INFO)
         analys_pass.grid(column=0, row=2, columnspan=2, sticky=tk.W)
+
+        self.result_label = analys_pass
 
         close = tk.Button(master=frame, text='Zamknij', width=10, command=frame.destroy)
         close.grid(column=0, row=3, pady=5, padx=10, sticky=tk.W)
 
-        check = tk.Button(master=frame, text='Sprawdz', width=10, command=self.write_password)
+        check = tk.Button(master=frame, text='Sprawdz', width=10, command=self.check_password_from_input)
         check.grid(column=1, row=3, pady=5, padx=10, sticky=tk.E)
 
-    def write_password(self, password):
-        encode_password = sha1(password.encode('utf-8'))
-        hash_password = encode_password.hexdigest()
-        prefix_password = hash_password[:5]
+    def get_password_string_from_input(self):
+        return self.INPUT.get()
+
+    def check_password(self, password_string):
+        encoded_password = sha1(password_string.encode('utf-8'))
+        hashed_password = encoded_password.hexdigest()
+        prefix_password = hashed_password[:5]
         url = 'https://api.pwnedpasswords.com/range/' + prefix_password
         response = get(url)
         response_lis = response.text.splitlines()
@@ -43,14 +50,25 @@ class Password:
         hash_list = [hash_list[0:35] for hash_list in response_lis]
         hash_count = [hash_list[36:] for hash_list in response_lis]
         hash_dict = dict(zip(hash_list, hash_count))
-        sufix_password = hash_password[5:40].upper()
+        sufix_password = hashed_password[5:40].upper()
 
         if sufix_password in hash_dict.keys():
-            text = f'Twoje hasło "{password}" było złamane i użyte {hash_dict[sufix_password]} razy. \n Zmień je jak najszybciej!!'
+            text = f'Twoje hasło "{password_string}" było złamane i użyte {hash_dict[sufix_password]} razy. \n Zmień je jak najszybciej!!'
 
         else:
-            text = f'Twoje hasło jest bezpieczne, tak trzymaj! \n hasło: {password} \n hasz: {hash_password} \n prefix: {prefix_password} \n sufix: {sufix_password}'
+            text = f'Twoje hasło jest bezpieczne, tak trzymaj! \n hasło: {password_string} \n hasz: {hashed_password} \n prefix: {prefix_password} \n sufix: {sufix_password}'
 
         return text
 
+    def check_password_from_input(self):
+        password_string = self.get_password_string_from_input()
+        result = self.check_password(password_string)
+        self.show_result(result)
+        print(result)
+        return result
 
+    def show_result(self, result):
+        self.TEXT_INFO.set(result)
+
+# passwordValidator = Password()
+# passwordValidator.open_frame()
